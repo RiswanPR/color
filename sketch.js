5415c707c8bf8018f6eb40c0ebb77e67233a1ad8
prriswan72 <88665690+prriswan72@users.noreply.github.com>
2021-09-15 12:52:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+function setup() {
+  createCanvas(400, 400);
+  background("grey")
+}
+
+
+function draw() {
+  
+  
+  if (keyDown(DOWN_ARROW))
+ {
+   background("blue");
+   
+ }
+ 
+ if (keyDown(UP_ARROW))
+ {
+   background("yellow");
+   
+ }
+ 
+   if (keyDown(LEFT_ARROW))
+ {
+   background("red");
+   
+ }
+ 
+ if (keyDown(RIGHT_ARROW))
+ {
+   background("black");
+   
+   
+ }
+  
+}
+ 
+
+
+  
+
+
+
+
+
+
